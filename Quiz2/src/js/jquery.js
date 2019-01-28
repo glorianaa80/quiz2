ad321bfb4e2c66b09f2c0c47f3d3389781024f63
Gloriana Angulo Alvarado <g.angulo@b7-09.local>
2019-01-28 12:56:29 -0600

    Agrego funcionalidad de el carusel

--- Quiz2/src/js/jquery.js
+++ Quiz2/src/js/jquery.js
@@ -1,85 +1,99 @@
-$(document).ready((evento) =>{ 
+
+
+
+$(document).ready(() => {
+
+  const request = new XMLHttpRequest();
+  request.addEventListener('load', (event) => {
+    const response = event.target.response.events
+    console.log(response)
+    $('h2').text(response[0].hardcoded)
+    $('#descrpcion0').text(response[0].name.text)
+    $('#locacion0').text(response[0].venue.name)
+    $('#btn-info').attr('href', response[0].url)
+    $('#imagen').attr('src', 'img/bg-event-1.jpeg')
+
     let indiceImg = 1;
     let info = 0;
 
-    $('h2').text('Jan 26th')
-    $('#descripcion0').text('ROCK to ICE clinic - Ourary Ice Climbing Festival')
-    $('#locacion0').text('- Ouray Ice Park')
-    $('#imagen').attr('src','img/bg-event-1.jpeg')
-
-
     // EVENTO 1 
-    $('#fecha').text('Jan 26th')
-    $('#descripcion').text('ROCK to ICE clinic - Ourary Ice Climbing Festival')
-    $('#locacion').text('- Ouray Ice Park')
+    $('#fecha').text(response[0].hardcoded)
+    $('#descripcion').text(response[0].name.text)
+    $('#locacion').text(response[0].venue.name)
+    $('#btn1').attr('href', response[0].url)
     // Evento 2 
-    $('#fecha2').text('Jan 30th - Feb 1st')
-    $('#descripcion2').text('Outdoor Retailer Snow Show')
-    $('#locacion2').text('- The Colorado Convention Center')
+    $('#fecha2').text(response[1].hardcoded)
+    $('#descripcion2').text(response[1].name.text)
+    $('#locacion2').text(response[1].venue.name)
+    $('#btn2').attr('href', response[1].url)
     // EVENTO 3
-    $('#fecha3').text('Feb 1st')
-    $('#descripcion3').text('Berthoud Backcountry Bonanza')
-    $('#locacion3').text('- Berthoud Pass')
+    $('#fecha3').text(response[2].hardcoded)
+    $('#descripcion3').text(response[2].name.text)
+    $('#locacion3').text(response[2].venue.name)
+    $('#btn3').attr('href', response[2].url)
     // EVENTO 4
-    $('#fecha4').text('Feb 3rd')
-    $('#descripcion4').text('FatBiking @ Devil´s Thumb Ranch')
-    $('#locacion4').text('- Devil´s Thumb Ranch Resort & Spa')
+    $('#fecha4').text(response[3].hardcoded)
+    $('#descripcion4').text(response[3].name.text)
+    $('#locacion4').text(response[3].venue.name)
+    $('#btn4').attr('href', response[3].url)
     // EVENTO 5
-    $('#fecha5').text('Feb 4th - Feb 5th')
-    $('#descripcion5').text('SIA/WWSRA On-Snow Demo')
-    $('#locacion5').text('- Cooper Mountain')
+    $('#fecha5').text(response[4].hardcoded)
+    $('#descripcion5').text(response[4].name.text)
+    $('#locacion5').text(response[4].venue.name)
+    $('#btn5').attr('href', response[4].url)
 
     // ANTERIOR
     $('#anterior').click(function anterior() {
-        indiceImg--;
-        if (indiceImg < 1) {
-          indiceImg = 5;
-        }
-        $('#imagen').attr('src', 'img/bg-event-'+indiceImg+'.jpeg')
+      indiceImg--;
+      if (indiceImg < 1) {
+        indiceImg = 5;
+      }
+      $('#imagen').attr('src', 'img/bg-event-' + indiceImg + '.jpeg')
 
-        // Cambio de información
-        info--;
-        if (info < 0) {
-          info = 4;
-        }
-        $('h2').text(response[info].hardcoded)
-        $('#descrpcion0').text(response[info].name.text)
-        $('#locacion0').text(response[info].venue.name);
-        $('btnInfo').attr('href', response[info].url);
-      })
+      // Cambio de información
+      info--
+      if (info < 0) {
+        info = 4;
+      }
+      $('h2').text(response[info].hardcoded)
+      $('#descrpcion0').text(response[info].name.text)
+      $('#locacion0').text(response[info].venue.name);
+      $('#btn-info').attr('href', response[info].url);
+    })
     //   SIGUIENTE
     $('#siguiente').click(function siguiente() {
-        indiceImg++;
-        if (indiceImg > 5) {
-          indiceImg = 1;
-        }
-        $('#imagen').attr('src', 'img/bg-event-'+indiceImg+'.jpeg')
-      
-        // Cambio de información
-        info++;
-        if (info > 4) {
-          info = 0;
-        }
-        document.querySelector('h2').innerHTML = response[info].hardcoded;
-        document.querySelector('#descrpcion0').innerHTML = response[info].name.text;
-        document.querySelector('#locacion0').innerHTML = response[info].venue.name;
-        btnInfo.setAttribute('href', response[info].url);
-      })
-      $('li').click(function activo() {
-        const lis = document.querySelectorAll('li');
-        for (const i of lis) {
-          i.addEventListener('click', () => {
-            const li = i;
-            for (const a of lis) {
-              a.className = 'info-fechas-lista';
-            }
-            li.className = 'info-fechas-lista bbline';
-          });
-        }
-      })
+      indiceImg++;
+      if (indiceImg > 5) {
+        indiceImg = 1;
+      }
+      $('#imagen').attr('src', 'img/bg-event-' + indiceImg + '.jpeg')
+
+      // Cambio de información
+      info++;
+      if (info > 4) {
+        info = 0;
+      }
+      $('h2').text(response[info].hardcoded)
+      $('#descrpcion0').text(response[info].name.text)
+      $('#locacion0').text(response[info].venue.name);
+      $('#btn-info').attr('href', response[info].url);
+    })
+    $('li').click(function activo() {
+      const lis = document.querySelectorAll('li');
+      for (const i of lis) {
+        i.addEventListener('click', () => {
+          const li = i;
+          for (const a of lis) {
+            a.className = 'info-fechas-lista';
+          }
+          li.className = 'info-fechas-lista bbline';
+        });
+      }
+    })
+  })
+  request.responseType = 'json';
+  request.open('GET', '/data.json');
+  request.send();
+
+
 })
-
-
-
-
-$('h1').css('color', 'coral')
